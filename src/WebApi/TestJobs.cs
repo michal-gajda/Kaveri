1237e2dd4d0715e2da47c5127b01d2b6c283aa48
Maria Romanowska <maria.m.romanowska@gmail.com>
2025-11-17 22:16:37 +0100

    Add test jobs

---
+++ src/WebApi/TestJobs.cs
@@ -0,0 +1,20 @@
+﻿using TickerQ.Utilities.Base;
+
+namespace Kaveri.WebApi;
+
+public class TestJobs
+{
+    [TickerFunction("TestJob")]
+    public Task TestJob(TickerFunctionContext context, CancellationToken cancellationToken)
+    {
+        Console.WriteLine($"TickerQ is working! Job ID: {context.Id}");
+        return Task.CompletedTask;
+    }
+
+    [TickerFunction("TestJobException")]
+    public async Task TestJob2(TickerFunctionContext context, CancellationToken cancellationToken)
+    {
+        await Task.Delay(TimeSpan.FromSeconds(10), cancellationToken);
+        throw new Exception("Test exception");
+    }
+}
